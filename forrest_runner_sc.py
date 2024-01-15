f66aaa6b7c1b253f31a5455f3870ef8492287160
Dominic Hale <dominichale05@outlook.com>
2024-01-15 21:14:15 +0000

    Completed window setup. Setup FPS counter

--- forrest_runner_sc.py
+++ forrest_runner_sc.py
@@ -1,26 +1,29 @@
 """
 Welcome to Forrest Runner source code! Project is programmed in python using pygame (along with other libraries).
-This project will form part of my OCR A-Level computer science NEA project. See supporting coursework writeup documents.
+This project will form part of my OCR A-Level computer science NEA project. See supporting coursework writeup documents for more info.
 
 Written by: Dominic H
-Candidate number: 9626
-Centre number: 18517
+Candidate number: *REDACTED*
+Centre number: *REDACTED*
 """
 
-#import necessary libraries used in program
+# import necessary libraries used in program
 import pygame
-import time as t
+from sys import exit
 
-#setup pygame and create window for game to run in
+# setup pygame and create window for game to run in
 pygame.init()
+DS1 = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption('Forrest Runner')  # sets window title
 
-DS1 = pygame.display.set_mode((2560,1440))
+clock = pygame.time.Clock()  # controls timing for the entire game
 
 while True:
-    
-    #allow user to exit from the game from the OS
+    # allow user to exit from the game from the OS
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit
-            
-    pygame.display.update() #draw elements and refresh the display
+            pygame.quit()
+            exit()
+
+    pygame.display.update()  # draw elements and refresh the display
+    clock.tick(60)  # sets framerate timer to 60fps
