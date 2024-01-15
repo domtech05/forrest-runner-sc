eb16d71c67a730dad15826504759c793004b05d1
Dominic Hale <dominichale01@gmail.com>
2024-01-15 18:41:24 +0000

    Added a comment to the start of the script

--- forrest_runner_sc.py
+++ forrest_runner_sc.py
@@ -1,1 +1,6 @@
+"""
+Welcome to Forrest Runner source code! Project is programmed in python using pygame (along with other libraries).
+This project will form part of my OCR A-Level computer science NEA project. See supporting coursework writeup documents.
+Written by: Dominic H - Candidate number: 9626 - Centre number: 18517
+"""
 import pygame
