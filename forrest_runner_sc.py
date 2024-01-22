2013fbff0bf513570b813ea8f29e3db372e426bf
domtech05 <dominichale01@gmail.com>
2024-01-22 22:33:39 +0000

    just made it better

--- forrest_runner_sc.py
+++ forrest_runner_sc.py
@@ -18,13 +18,9 @@ screen = pygame.display.set_mode((1920, 1080))  # sets the display size for pyga
 pygame.display.set_caption('Forrest Runner')  # sets window title show to user within host OS
 
 clock = pygame.time.Clock()  # Creates a clock variable that is constantly updated during execution. Used to control
+
+
 # all time dependant functions within the game.
-
-
-# FONT AND TEXT SETUP
-fontRegular = pygame.font.Font("CabinSketch-Regular.ttf", 20)  # define a regular size font to be used through the game
-fontBold = pygame.font.Font("CabinSketch-Bold.ttf", 20)  # define a bold font to be used for titles
-
 
 # Create function that will allow user to exit game from host OS. Must be called in every while loop!!
 def exitFunc():
@@ -35,12 +31,14 @@ def exitFunc():
 
 
 def mainMenu():
+    titleFont = pygame.font.Font("CabinSketch-Bold.ttf", 60)  # define a bold font to be used for the main title
+
     while True:
         # allow user to exit from the game from the OS
         exitFunc()
 
         screen.fill((255, 255, 255))  # TEMP
-        screen.blit(fontRegular.render("John Hubbard", True, (0, 0, 255)), (40, 140))  # TEMP
+        screen.blit(titleFont.render("John Hubbard", True, (0, 0, 255)), (40, 140))  # TEMP
 
         pygame.display.update()  # draw elements and refresh the display on every clock cycle
         clock.tick(60)  # controls how fast the game clock should run (in this case 60 times per second)
