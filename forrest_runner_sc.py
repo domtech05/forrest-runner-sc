e0f8831305062f4025a4c76ad8389a17dd7d8253
domtech05 <dominichale01@gmail.com>
2024-01-23 23:05:10 +0000

    Added assets folder. Began creating main menu. Cleaned up some other parts. Main menu still needs work

--- forrest_runner_sc.py
+++ forrest_runner_sc.py
@@ -16,10 +16,13 @@ from sys import exit
 pygame.init()  # initialize all pygame modules to avoid having initialize all separately
 width = 1920
 height = 1080
-screen = pygame.display.set_mode((width, height))  # sets the display size for pygame to render to (in this case # 1920*1080)
+screen = pygame.display.set_mode(
+    (width, height))  # sets the display size for pygame to render to (in this case # 1920*1080)
 pygame.display.set_caption('Forrest Runner')  # sets window title show to user within host OS
 
 clock = pygame.time.Clock()  # Creates a clock variable that is constantly updated during execution. Used to control
+
+
 # all time dependant functions within the game.
 
 
@@ -32,13 +35,40 @@ def exitFunc():
 
 
 def mainMenu():
-    titleFont = pygame.font.Font("CabinSketch-Bold.ttf", 100)
+    titleFont = pygame.font.Font("CabinSketch-Bold.ttf", 100)  # Load font for main title
+    taglineFont = pygame.font.Font("CabinSketch-Regular.ttf", 50)  # Load font for title taglines
+
+    background_image = pygame.image.load("ASSETS/background.jpg")  # Load background image from assets folder
+    background_image = pygame.transform.scale(background_image, (width, height))  # scale background image to fill
+
+    # screen
+
+    # Function to draw buttons to the screen
+    def drawButton(text, x, y, bWidth, bHeight):
+        pygame.draw.rect(screen, (255, 255, 255), (x, y, bWidth, bHeight))
+        pygame.draw.rect(screen, (0, 0, 0), (x, y, bWidth, bHeight), 2)
+
+        buttonFont = pygame.font.Font("CabinSketch-Regular.ttf", 70)
+
+        # Render text
+        #screen.blit(buttonFont.render(text, True, ))
+
+
+    # Main menu loop
     while True:
         # allow user to exit from the game from the OS
         exitFunc()
 
-        screen.fill((255, 255, 255))  # TEMP
-        screen.blit(titleFont.render("Forest runner", True, (0, 0, 0)), ((width / 2), (height / 2)))  # TEMP
+        screen.blit(background_image, (0, 0))  # Draw background image to screen
+
+        screen.blit(taglineFont.render("Welcome to", True, (0, 0, 0)),
+                    (width // 2 - taglineFont.size("Welcome to")[0] // 2, 290))  # Add tagline to top of title
+        screen.blit(titleFont.render("Forest runner", True, (0, 0, 0)),
+                    (width // 2 - titleFont.size("Forest runner")[0] // 2, 320))  # Draw main title to screen
+
+        button_width, button_height = 200, 50
+        button_start_y = height // 2
+        drawButton("Button 1", (width - button_width) // 2, button_start_y, button_width, button_height)
 
         pygame.display.update()  # draw elements and refresh the display on every clock cycle
         clock.tick(60)  # controls how fast the game clock should run (in this case 60 times per second)
