896b663fc16dd5aebbeae74c42147ba184fcc82a
domtech05 <dominichale01@gmail.com>
2024-01-28 15:43:04 +0000

    part way through sorting the button class thing

--- forrest_runner_main.py
+++ forrest_runner_main.py
@@ -38,38 +38,21 @@ def exitButton():
     exit()
 
 
+class button:
+    def __init__(self, x, y, bWidth, bHeight, image, font, text):
+
+
 
 def mainMenu():
-    titleFont = pygame.font.Font("CabinSketch-Bold.ttf", 100)  # Load font for main title
-    taglineFont = pygame.font.Font("CabinSketch-Regular.ttf", 50)  # Load font for title taglines
+    titleFont = pygame.font.Font("FONTS/CabinSketch-Bold.ttf", 100)  # Load font for main title
+    taglineFont = pygame.font.Font("FONTS/CabinSketch-Regular.ttf", 50)  # Load font for title taglines
 
     background_image = pygame.image.load("ASSETS/background.jpg")  # Load background image from assets folder
     background_image = pygame.transform.scale(background_image, (width, height))  # scale background image to fill
     # screen
 
 
-    # Function to draw buttons to the screen
-    def drawMenuButton(text, x, y, bWidth, bHeight, action):
-        buttonImage = pygame.image.load("ASSETS/button.png")  # load button image
-        buttonImage = pygame.transform.scale(buttonImage, (
-            bWidth, bHeight))  # scale button image to be correct size according to parameter passed in
-        buttonFont = pygame.font.Font("CabinSketch-Regular.ttf", 60)  # create button font
 
-        screen.blit(buttonImage, (x, y))  # draw the button image to the screen
-
-        # Render text
-        text_surface = buttonFont.render(text, True, (0, 0, 0))  # render the text according to parameter passed in
-        text_rect = text_surface.get_rect(center=(x + bWidth // 2, y + bHeight // 2))  # centre text to the button image
-        screen.blit(text_surface, text_rect)  # draw the text to the screen
-
-        # Click actions
-        mouse = pygame.mouse.get_pos()  # get the mouse position on screen and assign it to a variable
-        click = pygame.mouse.get_pressed()  # get the click status of the mouse and assign it to a variable
-
-        if x < mouse[0] < x + bWidth and y < mouse[1] < y + bHeight:  # detect if the mouse falls within boundary of
-            # the button
-            if click[0] == 1 and action is not None:  # detect if mouse is clicked
-                action()  # run action defined in function parameter
 
 
     # Main menu loop
@@ -87,16 +70,8 @@ def mainMenu():
         menuButtonSpacing = 30  # define the space between each menu button
         menuButtonHeight, menuButtonWidth = 75, 350  # define the size for each button
 
-        drawMenuButton("Start", ((width - menuButtonWidth) // 2), (height // 2) - 75, menuButtonWidth, menuButtonHeight,
-                       None)  # draw the start button to the screen
 
-        drawMenuButton("Settings", ((width - menuButtonWidth) // 2),
-                       ((height // 2) + menuButtonHeight + menuButtonSpacing) - 75, menuButtonWidth, menuButtonHeight,
-                       None)  # draw the settings button to the screen
 
-        drawMenuButton("Exit", ((width - menuButtonWidth) // 2),
-                       ((height // 2) + 2 * (menuButtonHeight + menuButtonSpacing)) - 75, menuButtonWidth,
-                       menuButtonHeight, exitButton)  # draw the exit button to the screen
 
         pygame.display.update()  # draw elements and refresh the display on every clock cycle
         clock.tick(60)  # controls how fast the game clock should run (in this case 60 times per second)
