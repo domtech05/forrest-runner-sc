1da1dd482b9f7927da030d599c08485cb5852791
Dominic <dominichale01@gmail.com>
2024-03-15 11:00:44 +0000

    physicsy shit

--- scripts/entities.py
+++ scripts/entities.py
@@ -1,7 +1,17 @@
 import pygame
 
 class physicsEntity:
-    def __init__(self, game, entityType, pos, size):
+    def __init__(self, game, entityType, pos, size): # pass in all variables and assign to attributes
         self.game = game
         self.type = entityType
         self.pos = list(pos)
+        self.size = size
+        self.velocity = [0,0]
+
+    def update(self, movement=(0, 0)):
+        frameMovement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1]) # calculate total movement for the frame
+        self.pos[0] += frameMovement[0]  # Update X position based on frame movement variable defined above
+        self.pos[1] += frameMovement[1]  # same as above but for Y coordinate
+
+    def render(self, screen):
+        screen.blit(self.game.assets['player'], )
