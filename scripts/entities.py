56c680f2f7d0ec4ca8d64712f33b0d8a1cea699e
domtech05 <dominichale01@gmail.com>
2024-04-05 11:13:20 +0100

    update all

--- scripts/entities.py
+++ scripts/entities.py
@@ -1,19 +1,20 @@
 import pygame
 
+
 class physicsEntity:
-    def __init__(self, game, entityType, pos, size): # pass in all variables and assign to attributes
+    def __init__(self, game, entityType, pos, size):  # pass in all variables and assign to attributes
         self.game = game
         self.type = entityType
         self.pos = list(pos)
         self.size = size
-        self.velocity = [0,0]
+        self.velocity = [0, 0]
 
     def update(self, movement=(0, 0)):
-        frameMovement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])  # calculate total movement for the frame
+        frameMovement = (
+        movement[0] + self.velocity[0], movement[1] + self.velocity[1])  # calculate total movement for the frame
 
-        self.velocity[1] = min(5, self.velocity[1] + 0.1) # take the lower of two numbers and apply it to velocity.
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)  # take the lower of two numbers and apply it to velocity.
         # Ensures velocity cannot go over 5
-
 
         self.pos[0] += frameMovement[0]  # Update X position based on frame movement variable defined above
         self.pos[1] += frameMovement[1]  # same as above but for Y coordinate
