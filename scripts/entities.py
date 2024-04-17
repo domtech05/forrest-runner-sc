0d2d46ba516868fc178abe25f528ac2be31b7c96
Dominic <dominichale01@gmail.com>
2024-04-17 21:29:38 +0100

    quick push

--- scripts/entities.py
+++ scripts/entities.py
@@ -16,28 +16,28 @@ class physicsEntity:
     def update(self, tilemap, movement=(0, 0)):
         self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
         
-        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        frameMovement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
         
-        self.pos[0] += frame_movement[0]
+        self.pos[0] += frameMovement[0]
         entityRect = self.rect()
         for rect in tilemap.physicsRectsAround(self.pos):
             if entityRect.colliderect(rect):
-                if frame_movement[0] > 0:
+                if frameMovement[0] > 0:
                     entityRect.right = rect.left
                     self.collisions['right'] = True
-                if frame_movement[0] < 0:
+                if frameMovement[0] < 0:
                     entityRect.left = rect.right
                     self.collisions['left'] = True
                 self.pos[0] = entityRect.x
         
-        self.pos[1] += frame_movement[1]
+        self.pos[1] += frameMovement[1]
         entityRect = self.rect()
         for rect in tilemap.physicsRectsAround(self.pos):
             if entityRect.colliderect(rect):
-                if frame_movement[1] > 0:
+                if frameMovement[1] > 0:
                     entityRect.bottom = rect.top
                     self.collisions['down'] = True
-                if frame_movement[1] < 0:
+                if frameMovement[1] < 0:
                     entityRect.top = rect.bottom
                     self.collisions['up'] = True
                 self.pos[1] = entityRect.y
