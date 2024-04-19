9143e8288ce159d15f7590599a3a168ae006ad60
Dominic <dominichale01@gmail.com>
2024-04-19 08:10:25 +0100

    Comments

--- main_menu.py
+++ main_menu.py
@@ -13,15 +13,20 @@ import pygame
 from sys import exit
 from level1 import level1Class
 
-class mainMenu:
+class mainMenu:  # create class for main menu functions
     def __init__(self):
-        pygame.init()
+        pygame.init()  # initialise pygame to be able to use its functions
+        
+        # define and create screen to render objects to
         self.screenWidth = 640
         self.screenHeight = 480
         self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight))
         pygame.display.set_caption('Forrest Runner')
+        
+        # Create a clock variable that is constantly updated during execution of the program
         self.clock = pygame.time.Clock()
         
+        # Define fonts and images that are used throughout the menu system
         self.titleFont = pygame.font.Font("FONTS/CabinSketch-Bold.ttf", 60)
         self.taglineFont = pygame.font.Font("FONTS/CabinSketch-Regular.ttf", 40)
         self.backgroundImage = pygame.image.load("ASSETS/Images/background.jpg")
@@ -29,20 +34,20 @@ class mainMenu:
         
         self.buttons = []
     
-    def add_button(self, text, x, y, width, height, action=None):
+    def add_button(self, text, x, y, width, height, action=None):  # function to set up variables for buttons
         buttonFont = pygame.font.Font("FONTS/Pangolin-Regular.ttf", 25)
         button = self.Button(text, x, y, width, height, buttonFont, "ASSETS/button.png", action)
         self.buttons.append(button)
     
-    def run(self):
+    def run(self):  # Main function to run the main menu
         while True:
             self.handle_events()
             self.draw()
             
-            pygame.display.update()
-            self.clock.tick(60)
+            pygame.display.update()  # update display and render objects
+            self.clock.tick(60)  # sets clock tick rate for program
     
-    def handle_events(self):
+    def handle_events(self):  # Event detection for button clicks
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
@@ -53,7 +58,7 @@ class mainMenu:
                         button.clicked = True
                         button.action()
     
-    def draw(self):
+    def draw(self):  # render all text and images to screen
         self.screen.blit(self.backgroundImage, (0, 0))
         self.screen.blit(self.taglineFont.render("Welcome to", True, (0, 0, 0)),
                          (self.screenWidth // 2 - self.taglineFont.size("Welcome to")[0] // 2, 110))
@@ -63,7 +68,7 @@ class mainMenu:
         for button in self.buttons:
             button.draw(self.screen)
     
-    class Button:
+    class Button:  # class to create and render buttons to the screen
         def __init__(self, text, x, y, bWidth, bHeight, font, imagePath, action=None):
             self.text = text
             self.x = x
@@ -80,14 +85,14 @@ class mainMenu:
             
             self.action = action
         
-        def draw(self, screen):
+        def draw(self, screen):  # draw buttons to screen
             screen.blit(self.buttonImage, (self.x, self.y))
             
             textSurface = self.buttonFont.render(self.text, True, (0, 0, 0))
             textRect = textSurface.get_rect(center=(self.x + self.width // 2, self.y + self.height // 2))
             screen.blit(textSurface, textRect)
             
-    def runLevel(self):
+    def runLevel(self):  # run level 1 when start is pressed
         level1Class(self.screen).run()
 
 if __name__ == "__main__":
@@ -97,4 +102,4 @@ if __name__ == "__main__":
     mainMenu.add_button("Settings", (mainMenu.screenWidth - 150) // 2, (mainMenu.screenHeight // 2) + 20, 150, 40)
     mainMenu.add_button("Exit", (mainMenu.screenWidth - 150) // 2, (mainMenu.screenHeight // 2) + 70, 150, 40, action=exit)
     
-    mainMenu.run()
+    mainMenu.run()  # run main menu
